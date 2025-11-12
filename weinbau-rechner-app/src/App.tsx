22f8960ef02c3586dd943b475a8972835eca485c
dmitrined <dm.nedioglo@gmail.com>
2025-11-12 21:44:21 +0100

    ok

---
+++ weinbau-rechner-app/src/App.tsx
@@ -0,0 +1,16 @@
+
+import type { JSX } from 'react'
+import './App.css'
+import WeinbauRechnerApp from './components/WeinbauRechnerApp/WeinbauRechnerApp'
+
+function App(): JSX.Element{
+  
+
+  return (
+    <div>
+      <WeinbauRechnerApp/>
+    </div>
+  )
+}
+
+export default App
